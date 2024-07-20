525ef4d39cc208514201ace5d1c36bdbcf74c218
Keerthick-1612 <keerthick2210372@ssn.edu.in>
2024-07-20 18:11:24 +0530

    new

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="py-8 px-16 border-t border-primary mt-8">
       <Image src={Logo} className="w-28 mb-4" alt="footerLogo"/>
-      <div>Copyright © 2024 SSN | ICAFM. All Rights Reserved</div>
+      <div>Copyright © 2024 SSN | ICAFM. All Rights  Reserved</div>
     </footer>
   );
 }
